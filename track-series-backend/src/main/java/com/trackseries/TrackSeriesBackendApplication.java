46360bd958cf7d04ba44dd01b058677329a19b85
lgim2372 <gergo.lazin123@gmail.com>
2026-03-01 21:39:09 +0200

    init spring project

---
+++ track-series-backend/src/main/java/com/trackseries/TrackSeriesBackendApplication.java
@@ -0,0 +1,13 @@
+package com.trackseries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackSeriesBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TrackSeriesBackendApplication.class, args);
+    }
+
+}
---
+++ track-series-backend/src/test/java/com/trackseries/TrackSeriesBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trackseries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackSeriesBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
